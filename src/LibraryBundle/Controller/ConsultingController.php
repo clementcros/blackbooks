8e3b0354e62fa36882cfef024d3751b33bfae259
clementcros <crosclement34500@gmail.com>
2017-03-13 14:19:43 +0100

    a

--- src/LibraryBundle/Controller/ConsultingController.php
+++ src/LibraryBundle/Controller/ConsultingController.php
@@ -3,48 +3,80 @@
 namespace LibraryBundle\Controller;
 
 use LibraryBundle\Entity\Books;
+use LibraryBundle\Entity\Categories;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class ConsultingController extends Controller
 {
     /**
+     * @Method ({"GET"})
      * @Route("/books2")
      */
     public function booksAction()
     {
         $entityManager = $this -> getDoctrine();
         $livres = $entityManager -> getRepository('LibraryBundle:Books') ->findAll();
-        return $this->render('LibraryBundle:Consulting:books.html.twig', array(
-            'livres' => $livres
-            // ...
-        ));
+      //  return $this->render('LibraryBundle:Consulting:books.html.twig', array(
+      //      'livres' => $livres
+       //
+       // ));
+        $tablivre = [];
+        foreach ($livres as $livre) {
+            $tablivre [] = [
+                'id' => $livre->getId(),
+                'titre' => $livre->getTitre(),
+                'auteur' => $livre->getAuteur(),
+            ];
+
+        }
+
+        return new JsonResponse($tablivre);
+
     }
 
     /**
-     * @Route("/categories/value/books")
+     * @Method({"GET"})
+     * @Route("/youpi")
      */
     public function catAction()
     {
 
-            // ...
+
             $entityManager = $this -> getDoctrine();
         $categories = $entityManager -> getRepository('LibraryBundle:Categories') ->findAll();
-        return $this->render('LibraryBundle:Consulting:cat.html.twig', array(
-            'categories' => $categories));
+        foreach ($categories as $categorie) {
+            $tabcat[] = [
+                'id' => $categorie->getId(),
+                'nom' => $categorie->getNom(),
+            ];
+        }
+        return new JsonResponse($tabcat);
+//        return $this->render('LibraryBundle:Consulting:cat.html.twig', array(
+//            'categories' => $categories));
 
     }
 
     /**
+     * @Method({"GET"})
      * @Route("/books/{id}")
      */
     public function bookAction(Books $book)
     {
 
 
-        return $this->render('LibraryBundle:Consulting:book.html.twig', array(
-            'book' => $book,
-        ));
+       // return $this->render('LibraryBundle:Consulting:book.html.twig', array(
+          //  'book' => $book,
+      //  ));
+        return new JsonResponse([
+            'id' =>$book->getId(),
+            'titre' =>$book->getTitre(),
+            'auteur' => $book->getAuteur(),
+
+        ]);
     }
 
 }
--- src/LibraryBundle/Controller/DefaultController.php
+++ src/LibraryBundle/Controller/DefaultController.php
@@ -2,8 +2,13 @@
 
 namespace LibraryBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use LibraryBundle\Entity\Categories;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
@@ -12,6 +17,66 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+
         return $this->render('LibraryBundle:Default:index.html.twig');
     }
+
+    /**
+     * @Route("/cat")
+     * @Method({"POST"})
+     */
+    public function catAction(Request $request){
+        //on definit un nouvelle objet
+        $categories = new Categories();
+        //on configure $categories pour qu'il recupere dans la requete la valeur 'nom'
+        $categories->setNom($request ->get('nom'));
+        //on crée un 'entity manager'
+        $em = $this->getDoctrine()->getManager();
+        //on se prepare a envoyer
+        $em->persist($categories);
+        //on envoie
+        $em->flush();
+        //on retourne la reponse au format json
+        return new JsonResponse($categories ->getNom());
+    }
+
+    /**
+     * @Route("/catedit")
+     * @Method({"PUT"})
+     */
+    public function editAction(Request $request){
+        //on cree un 'entity manager'
+        $em = $this->getDoctrine()->getManager();
+        //on recupere dans la table categorie celle avec l'id qu'on a defini dans l'url
+        $categories = $em->getRepository('LibraryBundle:Categories')->find($request ->get('id'));
+        //on configure $categories pour qu'il recupere dans la requete la valeur 'nom'
+        $categories->setNom($request->get('nom'));
+        //on se prepare a envoyer
+        $em->persist($categories);
+        //on envoie
+        $em->flush();
+        //on retourne la reponse au format json
+        return new JsonResponse($categories->getNom());
+        //c'est faut xd lol ptdr mdr jpp
+    }
+
+    /**
+     * @Route("/catdel")
+     * @Method({"DELETE"})
+     */
+    public function deletAction(Request $request) {
+    //on cree un 'entity manager'
+    $em = $this->getDoctrine()->getManager();
+    //on recup' dans la table cat celle avec l'id qu'on a défini dans l'URLLLLLLLL
+    $categories = $em->getRepository('LibraryBundle:Categories')->find($request ->get('id'));
+//        On suprr
+    $em->remove($categories);
+//        On envoie
+    $em->flush();
+//        On Observe
+    return new JsonResponse($categories);
 }
+
+
+}
+
